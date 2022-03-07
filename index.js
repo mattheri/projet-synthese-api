7283a7df0efa03995fa9e35b870fc0849283e949
Mathieu Thériault <mathieu.theriault89@gmail.com>
2022-03-07 18:19:18 -0500

    add helmet

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(require('cors')());
+app.use(require('helmet')())
 app.use(require('body-parser').json());
 
 const PORT = process.env.PORT || 3000;
