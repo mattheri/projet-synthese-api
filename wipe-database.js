d20146a92d39fd4bf6cbc30a392a4d6f84d45f07
Mathieu Thériault <mathieu.theriault89@gmail.com>
2022-03-10 08:52:17 -0500

    Added a way to drop all collections

--- wipe-database.js
+++ wipe-database.js
@@ -1,26 +1,32 @@
 const fs = require('fs/promises');
-const axios = require('axios').default;
-const students = require('./students');
+const string = require('./string');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 (async function () {
+	const URI = `mongodb+srv://${process.env.DB_ADMIN_USER}:${process.env.DB_ADMIN_PASSWORD}@projet-synthese.cy5wt.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+	const connection = mongoose.createConnection(URI);
 	const files = await fs.readdir(`./mock`);
-	const urls = students.flatMap((student) => {
 
-		const urls = files.map(file => process.env.API_URL ? `${process.env.API_URL}/api/${student}/${file.replace('.mock.js', '')}/wipe` : `http://localhost:3000/api/${student}/${file.replace('.mock.js', '')}/wipe`);
+	const normalizedFileNames = files.map(file => string.normalize(file.replace('.mock.js', '')));
 
-		return urls;
-	});
+	const isEnd = await Promise.all(normalizedFileNames.map(async (file) => {
+		console.log(`Dropping collection: ${file}`);
 
-	let i = 0;
+		const dropStatus = await connection.dropCollection(file);
 
-	const deleteData = (i) => {
-		axios.delete(urls[i]).then((result) => {
-			console.log('deleted', urls[i]);
-			console.log('result', result.status, result.data);
-			if (result.status === 200 && (i + 1) <= (urls.length - 1)) deleteData(i + 1);
-		})
+		if (dropStatus) console.log(`Collection ${file} dropped`);
+		else console.log(`Collection ${file} not dropped`);
+	}));
+
+	connection.close();
+	if (isEnd.every(Boolean)) {
+		console.log('All collections dropped');
+
+		process.exit(0);
 	}
 
-	deleteData(i);
-
+	process.exit(1);
 })();
