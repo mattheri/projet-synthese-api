08053e32c518be54c29bc292d968cebc4fae025f
Mathieu Thériault <mathieu.theriault89@gmail.com>
2022-04-10 16:07:27 -0400

    Augmented timeout

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const { PORT } = require("./params");
 const app = require('./app');
+const timeout = require("connect-timeout");
 
+function haltOnTimeout(req, res, next) {
+	if (!req.timedout) next();
+}
+
+app.use(timeout(120000));
 app.use(require('cors')());
 app.use(require("helmet")());
 app.use(require('body-parser').json());
 app.use(require('./api-router'));
+app.use(haltOnTimeout);
 
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
