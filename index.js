82937e8626219314a3012e65520a706339b721ef
Mathieu Thériault <m.theriault@novatize.com>
2022-04-12 19:15:36 -0400

    remove timeout

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 const { PORT } = require("./params");
-const app = require('./app');
-const timeout = require("connect-timeout");
+const app = require("./app");
 
-function haltOnTimeout(req, res, next) {
-	if (!req.timedout) next();
-}
-
-app.use(timeout(120000));
-app.use(require('cors')());
+app.use(require("cors")());
 app.use(require("helmet")());
-app.use(require('body-parser').json());
-app.use(require('./api-router'));
-app.use(haltOnTimeout);
+app.use(require("body-parser").json());
+app.use(require("./api-router"));
 
 app.listen(PORT, () => {
-	console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
